aa7d05a24270343d1356eb6af4dbce19effb08c3
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-03 17:29:39 +0200

    Input favorite color

--- myscript.js
+++ myscript.js
@@ -4,16 +4,17 @@ document.getElementById( 'title' ).innerHTML ='Welcome to Boolean';
 // assegnazione valori let
 let user_name = 'Nome';
 let user_surname = 'Cognome';
+let favorite_color = 'Colore'
 
 // ask the user to enter their own name;
 document.getElementById( 'name' ).innerHTML = user_name;
 
-
 //ask the user to enter their own surname; 
 document.getElementById( 'surname' ).innerHTML = user_surname;
 
-
 //ask the user to enter their own favorite colour;
+document.getElementById( 'fav_color' ).innerHTML = favorite_color;
+
 
 
 
