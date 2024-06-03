2cbb9ab55b9244a514827d68357c0a14933c9b3a
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-03 17:15:00 +0200

    Input nome e cognome

--- myscript.js
+++ myscript.js
@@ -1,10 +1,19 @@
-// ask the user to enter their own name 
+//Welcome page; 
+document.getElementById( 'title' ).innerHTML ='Welcome to Boolean';
+
+// assegnazione valori let
+let user_name = 'Nome';
+let user_surname = 'Cognome';
+
+// ask the user to enter their own name;
+document.getElementById( 'name' ).innerHTML = user_name;
 
 
-//ask the user to enter their own surname 
+//ask the user to enter their own surname; 
+document.getElementById( 'surname' ).innerHTML = user_surname;
 
 
-//ask the user to enter their own favorite colour
+//ask the user to enter their own favorite colour;
 
 
 
